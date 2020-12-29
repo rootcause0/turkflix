27b4a18123d950ec2db17e8268ef0623a9a05cab
rootcause0 <gorkem.coban@kredirobotu.com>
2020-12-29 18:10:42 -0500

    Migrations,npm packets and Subpage component appended with original_title field from shows table

--- backend/database/migrations/2020_12_15_090131_create_episodes_table.php
+++ backend/database/migrations/2020_12_15_090131_create_episodes_table.php
@@ -12,8 +12,13 @@ class CreateEpisodesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('season_number');
-            $table->integer('episode_number');;
+            $table->integer('episode_number');
+            $table->text('src')->nullable();
             $table->integer('seen')->default(0);
+            $table->bigInteger('show_id')->unsigned();
+            $table->bigInteger('season_id')->unsigned();
+            $table->foreign('show_id')->references('id')->on('shows');
+            $table->timestamps();
         });
     }
 
